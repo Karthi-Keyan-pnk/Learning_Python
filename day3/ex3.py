3213a3caa95d40fe0dd1e3cebd57a5c1a98ace06
Karthi <karthikeyan.pnk30@gmail.com>
2026-07-15 23:27:36 +0530

    day3 changes

--- day3/ex3.py
+++ day3/ex3.py
@@ -14,5 +14,36 @@ class Employee:
         self._salary = new_salary
 
 employee = Employee("John", 50000)
-employee.salary = -70000
+employee.salary = 70000
 print(employee.salary)
+
+
+class Employee:
+    def work(self):
+        print("Working")
+
+
+class Developer(Employee):
+    def write_code(self):
+        print("Writing code")
+
+Employee1 = Developer()
+Employee1.work()
+Employee1.write_code()
+
+
+class Engine:
+    def start(self):
+        print("Engine started")
+
+
+class Car:
+    def __init__(self):
+        self.engine = Engine()
+
+    def start(self):
+        self.engine.start()
+
+
+car = Car()
+car.start()
